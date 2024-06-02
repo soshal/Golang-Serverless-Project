3b38ef614908987c41a01834aa7cc05b0d543ad7
soshal <111147773+soshal@users.noreply.github.com>
2024-06-02 23:15:02 +0530

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,46 +1,51 @@
 package main
 
-import(
-"github.com/akhil/go-serverless-yt/pkg/handlers"
-"os"
-"github.com/aws/aws-lambda-go/events"
-"github.com/aws/aws-lambda-go/lambda"
-"github.com/aws/aws-sdk-go/aws"
-"github.com/aws/aws-sdk-go/aws/session"
-"github.com/aws/aws-sdk-go/service/dynamodb"
-"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+import (
+	"log"
+	"os"
+
+	"github.com/akhil/go-serverless-yt/pkg/handlers"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
-var(
+var (
 	dynaClient dynamodbiface.DynamoDBAPI
 )
 
-func main(){
+func main() {
 	region := os.Getenv("AWS_REGION")
-	awsSession, err := session.NewSession(&aws.Config{
-		Region: aws.String(region)},)
-
-	if err!=nil{
-		return
+	if region == "" {
+		log.Fatal("AWS_REGION environment variable not set")
 	}
+
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+	if err != nil {
+		log.Fatalf("Failed to create AWS session: %s", err)
+	}
+
 	dynaClient = dynamodb.New(awsSession)
 	lambda.Start(handler)
 }
 
-
 const tableName = "go-serverless-yt"
 
-func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error){
-		switch req.HTTPMethod{
-		case "GET":
-			return handlers.GetUser(req, tableName, dynaClient)
-		case "POST":
-			return handlers.CreateUser(req, tableName, dynaClient)
-		case "PUT":
-			return handlers.UpdateUser(req, tableName, dynaClient)
-		case "DELETE":
-			return handlers.DeleteUser(req, tableName, dynaClient)
-		default:
-			return handlers.UnhandledMethod()
-		}
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	methodHandlers := map[string]func(events.APIGatewayProxyRequest, string, dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error){
+		"GET":    handlers.GetUser,
+		"POST":   handlers.CreateUser,
+		"PUT":    handlers.UpdateUser,
+		"DELETE": handlers.DeleteUser,
+	}
+
+	if handlerFunc, exists := methodHandlers[req.HTTPMethod]; exists {
+		return handlerFunc(req, tableName, dynaClient)
+	}
+	return handlers.UnhandledMethod()
 }
